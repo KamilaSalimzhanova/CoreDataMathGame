39d26cef49e5d9e2c33487b97a5d5a99ea52330a
kamila <ksalimzh@gmail.com>
2025-04-03 13:50:11 +0500

    Initial Commit

---
+++ CoreDataMathGame/CoreDataMathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMathGameApp.swift
+//  CoreDataMathGame
+//
+//  Created by kamila on 03.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
