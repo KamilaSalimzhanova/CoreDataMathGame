47bddf4b9ca4f373f013e6c3a4ee902279dcb1aa
kamila <ksalimzh@gmail.com>
2025-04-03 15:04:15 +0500

    markdown comments and core data main functionaries finished with persistent controller

--- CoreDataMathGame/ViewModels/HighScoreViewModel.swift
+++ CoreDataMathGame/ViewModels/HighScoreViewModel.swift
@@ -2,13 +2,33 @@ import Foundation
 import CoreData
 
 final class HighScoreViewModel: ObservableObject {
-    private let context = PersistenceController.shared.viewContext 
     @Published var highScores: [HighScoreEntity] = []
-
+    private let context = PersistenceController.shared.viewContext
+    
+    // MARK: - Init function
     init() {
         fetchHighScores()
     }
+
+    // MARK: - Public functions
+    func addHighScore(name: String, highScore: Int64) {
+        let entity = HighScoreEntity(context: context)
+        entity.name = name
+        entity.score = highScore
+        saveHighScore()
+    }
     
+    func updateEntity(_ entity: HighScoreEntity, newName: String) {
+        entity.name = newName
+        saveHighScore()
+    }
+    
+    func deleteHighScore(_ entity: HighScoreEntity) {
+        context.delete(entity)
+        saveHighScore()
+    }
+    
+    // MARK: - Private functions
     private func fetchHighScores() {
         let request: NSFetchRequest<HighScoreEntity> = HighScoreEntity.fetchRequest()
         request.sortDescriptors = [NSSortDescriptor(key: "score", ascending: false)]
@@ -18,7 +38,7 @@ final class HighScoreViewModel: ObservableObject {
             print("Error fetching high scores: \(error.localizedDescription)")
         }
     }
-    
+
     private func saveHighScore() {
         do {
             try context.save()
@@ -27,4 +47,5 @@ final class HighScoreViewModel: ObservableObject {
             print("Error saving high score")
         }
     }
+    
 }
